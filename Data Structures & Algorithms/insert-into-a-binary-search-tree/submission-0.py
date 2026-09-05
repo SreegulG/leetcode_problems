ab5e07cf8b060f1bf27eb06b85fcb46a2de6a194
SreegulG <sreegulge@gmail.com>
2026-09-05 16:06:54 +0530

    Add: insert-into-a-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-0.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        
+        curr = root
+        if curr.val < val:
+            curr.right =  self.insertIntoBST(root.right, val)
+        if curr.val > val:
+             curr.left =  self.insertIntoBST(root.left, val)
+        return root
